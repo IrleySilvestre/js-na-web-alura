007c01031ad9146d5c422e1bf9e53fe167919c5d
Irley José Silvestre <irleysilvestre.mba@gmail.com>
2021-03-12 15:06:25 -0300

    incluirdo botao excluir

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,39 +1,57 @@
-const criarTarefa = (evento) => {
+(() => {
 
-    evento.preventDefault()
+    const criarTarefa = (evento) => {
 
-    const lista = document.querySelector('[data-list]')
-    const input = document.querySelector('[data-form-input]')
-    const valor = input.value
+        evento.preventDefault()
 
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
-    const conteudo = `<p class="content">${valor}</p>`
+        const lista = document.querySelector('[data-list]')
+        const input = document.querySelector('[data-form-input]')
+        const valor = input.value
 
-    tarefa.innerHTML = conteudo
+        const tarefa = document.createElement('li')
+        tarefa.classList.add('task')
+        const conteudo = `<p class="content">${valor}</p>`
 
-    tarefa.appendChild(btnComcluir())
-    lista.appendChild(tarefa)
-    input.value = " "
+        tarefa.innerHTML = conteudo
 
-}
-const novaTarefa = document.querySelector('[data-form-button]')
+        tarefa.appendChild(BotaoConcluir())
+        tarefa.appendChild(BotaoExcluir())
+        lista.appendChild(tarefa)
+        input.value = " "
 
-novaTarefa.addEventListener('click', criarTarefa)
+    }
+    const novaTarefa = document.querySelector('[data-form-button]')
 
-const btnComcluir = ()=>{
-    const btnconcluir = document.createElement('button')
-    btnconcluir.addEventListener('click', concluirTarefa)
-    btnconcluir.classList.add('check-button')
-    btnconcluir.innerText = 'Concluir'
-    return btnconcluir
-}
+    novaTarefa.addEventListener('click', criarTarefa)
 
-const concluirTarefa = (evento) =>{
-    const btnClicado = evento.target
-    const tarefaCompleta = btnClicado.parentElement
-    tarefaCompleta.classList.toggle('done')
-}
+    const BotaoConcluir = () => {
+        const btnconcluir = document.createElement('button')
+        btnconcluir.addEventListener('click', concluirTarefa)
+        btnconcluir.classList.add('check-button')
+        btnconcluir.innerText = 'Concluir'
+        return btnconcluir
+    }
+
+    const concluirTarefa = (evento) => {
+        const btnClicado = evento.target
+        const tarefaCompleta = btnClicado.parentElement
+        tarefaCompleta.classList.toggle('done')
+    }
+
+    const BotaoExcluir = ()=>{
+        const botaoExcluir = document.createElement('button')
+        botaoExcluir.innerText = 'Excluir'
+        botaoExcluir.classList.add('check-button')
+        botaoExcluir.addEventListener('click', excluirTarefa)
+        return botaoExcluir
+    }
+
+    const excluirTarefa = (evento)=>{
+        const btnClicado = evento.target
+        const tarefaApagar = btnClicado.parentElement
+        tarefaApagar.remove()
+    }
+})()
 
 
 
